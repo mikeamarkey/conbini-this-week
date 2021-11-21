7c9f86aafbc77cb95ac9c31aa8b0b4ba6c879067
Michael Markey <michaelamarkey@gmail.com>
2021-11-21 11:51:09 +0900

    fix: allow logging in scraper

--- packages/scraper/index.ts
+++ packages/scraper/index.ts
@@ -4,7 +4,6 @@ import 'dotenv/config'
 async function main() {
   const conbiniName = 'familymart'
   const result = await scrape(conbiniName)
-  // eslint-disable-next-line no-console
   console.log(result)
 }
 
