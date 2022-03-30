691be7c7ac7b716d30631e6306c01b4ec3240f89
Michael Markey <michaelamarkey@gmail.com>
2022-03-30 22:30:18 +0900

    feat: (web) switch to using next-image for images (#14)
    * chore: add jsdoc to next.config.js
    
    * feat: switch to using nextjs for images

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,5 +1,19 @@
-module.exports = {
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
   experimental: {
     externalDir: true,
   },
+  images: {
+    domains: [
+      'www.lawson.co.jp',
+      'img.7api-01.dp1.sej.co.jp',
+      'www.family.co.jp',
+    ],
+  },
 }
+
+module.exports = nextConfig
