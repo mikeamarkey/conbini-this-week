40a3eba7e3ad4918e3fc816f6bf3aba98eba3907
Michael Markey <michaelamarkey@gmail.com>
2022-05-21 05:10:18 +0900

    feat: add footer

---
+++ packages/web/src/components/Footer/index.tsx
@@ -0,0 +1,26 @@
+import { Box, Link } from 'components/Primitives'
+import { repoUrl } from '../../constants'
+import Image from 'next/image'
+import { Text } from '@nextui-org/react'
+
+export default function Footer() {
+  return (
+    <Box css={{ textAlign: 'center' }}>
+      <Link href={repoUrl}>
+        <Image
+          width={24}
+          height={24}
+          src="/github.png"
+          alt="Conbini This Week"
+        />
+      </Link>
+      <Text size="$xs" css={{ marginTop: '$xs' }}>
+        {`This project is made with ❤️ for the conbini.`}
+        <br />
+        {`All images and text belong to the respective companies.`}
+        <br />
+        {`(Please don't copyright strike this 🙏)`}
+      </Text>
+    </Box>
+  )
+}
