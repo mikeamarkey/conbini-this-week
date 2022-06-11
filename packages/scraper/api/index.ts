f3c0544d471aa3f59317d5002b56b982283f8e5f
Michael Markey <michaelamarkey@gmail.com>
2022-06-11 16:22:43 +0900

    feat: add basic authorization for api endpoint

--- packages/scraper/api/index.ts
+++ packages/scraper/api/index.ts
@@ -3,6 +3,12 @@ import { scrape, scrapeAll } from '../src/scrape'
 import { isValidConbiniName } from '../src/utils'
 
 export default async function (req: VercelRequest, res: VercelResponse) {
+  const { authorization } = req.headers
+  if (authorization !== `Bearer ${process.env.API_SECRET_KEY}`) {
+    res.status(401).send('Unauthorized')
+    return
+  }
+
   const { name } = req.query
   if (!name || typeof name !== 'string') {
     res.status(400).send('Please provide a correct name')
@@ -24,6 +30,7 @@ export default async function (req: VercelRequest, res: VercelResponse) {
       res.status(200).send(`${result} items from ${name} added`)
     }
   } catch (e) {
+    console.log(e)
     res.status(500).send('Something went wrong...')
   }
 }
