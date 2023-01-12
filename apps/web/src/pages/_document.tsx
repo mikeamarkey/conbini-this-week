58a57df82f84e85d9f26e852ff303754c38e2b85
Michael Markey <michaelamarkey@gmail.com>
2023-01-13 06:32:16 +0900

    chore: working on SSRProvider error from nextui

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Document, {
   Html,
   Head,
@@ -13,19 +14,22 @@ export default class MyDocument extends Document {
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps, styles: <>{initialProps.styles}</> }
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    }
   }
 
   render() {
     return (
       <Html lang="ja-jp">
         <Head>
-          <meta charSet="utf-8" />
           {CssBaseline.flush()}
+          <meta charSet="utf-8" />
           <link
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏪</text></svg>"
-          ></link>
+          />
         </Head>
         <body>
           <Main />
