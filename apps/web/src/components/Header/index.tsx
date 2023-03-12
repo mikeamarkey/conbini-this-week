14d31cb7452aeaacfc8dc680e9a976397a0b74dd
Michael Markey <michaelamarkey@gmail.com>
2023-03-12 10:16:59 +0900

    chore: migrate header

--- apps/web/src/components/Header/index.tsx
+++ apps/web/src/components/Header/index.tsx
@@ -1,29 +1,12 @@
-import { styled, Text } from '@nextui-org/react'
-
-const Wrapper = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-})
-
-const Title = styled('h1', {
-  fontSize: '$4xl',
-  '@sm': {
-    fontSize: '$5xl',
-  },
-})
-
 export type HeaderProps = {
   itemCount: number
 }
 
 export default function Header({ itemCount }: HeaderProps) {
   return (
-    <Wrapper>
-      <Title>Conbini This Week</Title>
-      <Text size="$xl" weight="bold">
-        {itemCount} new items this week!
-      </Text>
-    </Wrapper>
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl sm:text-5xl">Conbini This Week</h1>
+      <p className="text-xl font-bold">{itemCount} new items this week!</p>
+    </div>
   )
 }
